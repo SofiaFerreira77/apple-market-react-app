e79aee61e8d2ed6b17d83b835113f6888ee0dd85
Sofia Ferreira <sofia.ferreira@impactcommerce.pt>
2023-10-26 18:14:10 +0100

    separate repository and usecases

--- src/components/ProductList.js
+++ src/components/ProductList.js
@@ -1,17 +1,39 @@
+import { useEffect, useState } from "react"
 import { isArrayEmpty } from "../utils/Utils"
 import styles from '../styles/ProductList.module.css'
 import ProductItem from "./ProductItem"
+import { getProductsCase } from "../useCases/ProductUseCase";
 
 export default function List() {
-    const productsList = Object.entries(ProductListData.items)
-    const products = Object.values(productsList);
+    const [products, setProducts] = useState([]);
+    const [loading, setLoading] = useState(false);
 
-    const showProductList = isArrayEmpty(products) ? [] : products.map((product) => <ProductItem key={product.id} product={product[1]} />)
+    async function fetchData() {
+        const data = await getProductsCase() || [];
+        setLoading(true);
+
+        try {
+            setProducts(data)
+            setLoading(false);
+        } catch (error) {
+            console.error('Error fetching products:', error);
+            setProducts([])
+            setLoading(false);
+        }
+    }
+
+    useEffect(() => {
+        fetchData();
+    }, [])
 
     return (
         <main className={styles.ProductList}>
-            <div className={styles.ProductListContainer}>
-                {showProductList}
+            <div className={styles.ProductListContainer + ' container'}>
+                {isArrayEmpty(products) || loading ?
+                    'Loading...'
+                    :
+                    products?.map((product) => <ProductItem key={product.id} product={product} />)
+                }
             </div>
         </main>
     )
